99b39acb6d0714b69118d92573d960258a3b8302
Keen <keencwilson@gmail.com>
2021-08-10 14:59:10 -0700

    Take two numeric values and then perform one of four operations

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-go-calculator
+# Advanced Calculator
+
+This advanced calculator could take two numeric values and then perform one of four operations: addition, subtraction, multiplication or division.
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Create a single reader object
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+	value1 := getInput("Value 1")
+	value2 := getInput("Value 2")
+
+	var result float64
+
+	switch operation := getOperation(); operation {
+	case "+":
+		result = addValues(value1, value2)
+	case "-":
+		result = subtractValues(value1, value2)
+	case "*":
+		result = multiplyValues(value1, value2)
+	case "/":
+		result = divideValues(value1, value2)
+	default:
+		panic("invalid operation")
+	}
+	fmt.Printf("The result is %v\n\n", result)
+
+}
+
+func doSomething() {
+	fmt.Println("Doing something")
+}
+
+func addAllValues(values ...int) (int, int) {
+	total := 0
+	for _, v := range values {
+		total += v
+	}
+	return total, len(values)
+}
+
+func getInput(propmt string) float64 {
+	fmt.Printf("%v ", propmt)
+	input, _ := reader.ReadString('\n')
+	value, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	if err != nil {
+		message := fmt.Sprintf("%v must be a number", propmt)
+		panic(message)
+	}
+	return value
+}
+
+func getOperation() string {
+	fmt.Print("Select an operation (+ - * /): ")
+	op, _ := reader.ReadString('\n')
+	return strings.TrimSpace((op))
+}
+
+func addValues(value1, value2 float64) float64 {
+	return value1 + value2
+}
+
+func subtractValues(value1, value2 float64) float64 {
+	return value1 - value2
+}
+
+func multiplyValues(value1, value2 float64) float64 {
+	return value1 * value2
+}
+
+func divideValues(value1, value2 float64) float64 {
+	return value1 / value2
+}
